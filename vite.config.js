0d37823b272ac4dafe19b2ddc33e3dab0a3e31b1
dwoloszin <3h2so4@gmail.com>
2026-02-05 20:59:58 -0300

    dfdfdf

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PricePilot/', // Correct base path for GitHub Pages repository
+  base: '/PricePilot2/', // Correct base path for GitHub Pages repository
   plugins: [
     react(),
   ],
